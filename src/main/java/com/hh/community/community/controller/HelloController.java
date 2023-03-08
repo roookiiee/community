a601765c388a6ef6ae03da1a7827575a7291a470
huanghao <huanghao710@qq.com>
2023-03-08 13:27:10 +0800

    add check cookies

--- src/main/java/com/hh/community/community/controller/HelloController.java
+++ src/main/java/com/hh/community/community/controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.hh.community.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello(){
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String hello(){
+//        return "index";
+//    }
 
 }
--- src/main/java/com/hh/community/community/mapper/UserMapper.java
+++ src/main/java/com/hh/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.hh.community.community.mapper;
 import com.hh.community.community.Model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,4 +12,7 @@ public interface UserMapper {
 
     @Insert("INSERT INTO USER (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 }
